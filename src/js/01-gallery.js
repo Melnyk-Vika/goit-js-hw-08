b962e2959379ab31cd8fdc811e941f3733792140
Melnyk Viktoriia <vikavmelnyk@gmail.com>
2023-01-31 00:16:12 +0200

    chandes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,7 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
